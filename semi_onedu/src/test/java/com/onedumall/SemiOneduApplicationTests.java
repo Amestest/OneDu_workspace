67ab7df1b2f5101403f0525559c61b4d35dbb290
Amestest <gktk8411@naver.com>
2024-06-19 15:05:26 +0900

    semi_onedu

---
+++ semi_onedu/src/test/java/com/onedumall/SemiOneduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onedumall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiOneduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
