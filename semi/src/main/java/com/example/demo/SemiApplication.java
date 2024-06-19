6b22092feffa9f5e9999401efed41fbac358f160
Wonki11 <wongi5011@daum.net>
2024-06-19 17:58:15 +0900

    test

---
+++ semi/src/main/java/com/example/demo/SemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiApplication.class, args);
+	}
+
+}
