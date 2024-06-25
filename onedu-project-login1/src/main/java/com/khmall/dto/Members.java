02b886cf9763d78f6a851ab67bb04260dce968ef
kimjinwoo23 <heastoria@naver.com>
2024-06-25 15:52:32 +0900

    login

---
+++ onedu-project-login1/src/main/java/com/khmall/dto/Members.java
@@ -0,0 +1,20 @@
+package com.khmall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Members {
+
+	
+	private String members_id;
+	private String members_pw;
+}
