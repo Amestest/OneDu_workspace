c38141e966f14232cfc6c033b37f7cf4f4c768b1
Amestest <gktk8411@naver.com>
2024-07-01 14:27:02 +0900

    fusion

--- semi_onedu/src/main/java/com/onedumall/controller/MembersController.java
+++ semi_onedu/src/main/java/com/onedumall/controller/MembersController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.onedumall.dto.Members;
-
+import com.onedumall.dto.Products;
 import com.onedumall.service.RegisterService;
+
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -42,11 +43,20 @@ public class MembersController {
 	public String insertRegister(Members members, Model model) {
 		log.info("1"+members.getMembers_id());
 		registerService.insertRegister(members);
-		log.info("2");
+		log.info("2"+members.getMembers_pw());
 		model.addAttribute("msg","회원가입이 성공적으로 완료됐습니다.");		
 		return "main";
 		
 	}
-	
-	
+	@GetMapping("/main")
+	public String showSearchForm(Model model) {
+		return "main";
+	}
+	@PostMapping("/main")
+	public String searchProducts(Model model, @RequestParam("keyword")String keyword) {
+		
+		List<Products> product = productService.searchProducts(keyword);
+		model.addAttribute("result" , product);
+		return "main";
+	}
 }
